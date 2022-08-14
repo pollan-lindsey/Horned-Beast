29612c535aa61343ff2358c735824b077baf21e5
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-14 13:05:47 -0500

    as close as I'll get

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+// import Carousel from 'react-bootstrap/Carousel';
+
 
 export default class HornedBeast extends Component {
    constructor(props) {
@@ -27,17 +30,37 @@ export default class HornedBeast extends Component {
 
    render() {
       return (
-         <div>
-         <h2>{this.props.title}</h2>
-         <img onClick={this.handleClicks} src={this.props.imageUrl}  alt='' title={this.props.title}></img>
-         <p> {this.state.likes} Likes</p>
-         <p>{this.props.description}</p>
-         {/* <h2 onClick={this.console}>click</h2>
-         <button onClick={this.console()}>
-         {/* <img  alt={this.props.title} /> */}
-         {/* </button>  */} 
+         // <div>
+         // <h2>{this.props.title}</h2>
+         // <img onClick={this.handleClicks} src={this.props.imageUrl}  alt='' title={this.props.title} width='420 px' height='350 px'></img>
+         // <p> {this.state.likes}Likes</p>
+         // <p>{this.props.description}</p>
+       
          
-         </div>
+         // </div>
+         // <div> 
+         // <Carousel>
+         //    <Carousel.Item>
+         //       <img onClick={this.handleClicks} src={this.props.imageUrl}  alt='' title={this.props.title} width='420 px' height='350 px' />
+         //       <Carousel.Caption>
+         //       <h4>{this.state.likes}Likes</h4>
+         //       <h2>{this.props.title}</h2>
+         //       <p>{this.props.description}</p>
+         //       </Carousel.Caption>
+         //    </Carousel.Item>
+         // </Carousel>
+         // </div>
+
+         <Card border='info' style={{ width: '18rem' }}>
+         <Card.Img onClick={this.handleClicks} src={this.props.imageUrl}  alt='' title={this.props.title} width='420 px' height='350 px' />
+         <Card.Body>
+           <Card.Title>{this.props.title}</Card.Title>
+           <Card.Subtitle className="mb-2 text-muted">{this.state.likes}Likes</Card.Subtitle>
+           <Card.Text>
+             {this.props.description}
+           </Card.Text>
+         </Card.Body>
+       </Card>
       )
    };
 }
